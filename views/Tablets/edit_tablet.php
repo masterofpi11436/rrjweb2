7de4a13e77f25ba52c6e1b326013f48e5eff47bf
Mark Tuggle <masterofpi11436@gmail.com>
2024-06-05 05:19:59 -0400

    Corrected styling

--- views/Tablets/edit_tablet.php
+++ views/Tablets/edit_tablet.php
@@ -6,6 +6,11 @@
     <button>Delete Tablet</button>
 </form>
 
+<form>
+    <label for="paste-input">Paste Info:</label>
+    <input type="text" id="paste-input" oninput="parseInput()" placeholder="[number] [last name], [first name] [middle name]">
+</form>
+
 <form method="post" action="/tablets/update/<?= htmlspecialchars($tablet['id']) ?>">
     <?php require "tablet_form.php"; ?>
 </form>
