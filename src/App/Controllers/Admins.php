8a3ed812c385c56fbd25609af724086e973d7537
Mark Tuggle <masterofpi11436@gmail.com>
2024-06-04 22:57:59 -0400

    Completed Admin validation

--- src/App/Controllers/Admins.php
+++ src/App/Controllers/Admins.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Security\User;
+use App\Models\User;
 use Framework\Viewer;
 use Framework\Exceptions\PageNotFoundException;
 use Framework\Controller;
