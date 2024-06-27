cbe61814df42670ffa28d58afea42de532e7433b
masterofpi11436 <masterofpi11436@gmail.com>
2024-06-27 14:39:28 -0400

    Changed Color Styling for Edge Browsers

--- views/Shared/header.php
+++ views/Shared/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
     <link rel="stylesheet" href="/public/css/styles.css">
+    <!--[if IE]><link rel="stylesheet" href="/public/css/edge.css"><![endif]-->
     <script src="/public/JavaScript/script.js"></script>
     <title><?= $title ?></title>
 </head>
