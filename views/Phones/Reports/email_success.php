59ddd18d3dbe175a5795aa7bea8bcb505cf1fe33
masterofpi11436 <masterofpi11436@gmail.com>
2024-08-07 13:14:19 -0400

    Added Back Button for phone directory

--- views/Phones/Reports/email_success.php
+++ views/Phones/Reports/email_success.php
@@ -1,2 +1,5 @@
 <h2>Thank you, the party responsible for updating the phone directory has been notified.</h2>
 <h2>If you do not see the requested entry updated within 1 business day. Please call Mark in MIU at ext 6035.</h2>
+<form action="/phones/directory">
+    <button>Back to Phone Directory</button>
+</form>
