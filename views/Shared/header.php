2a420802ece1a426eafa4481d38dab207686986c
Mark Tuggle <masterofpi11436@gmail.com>
2024-08-12 14:41:35 -0400

    Added Meta tag for IE Mode

--- views/Shared/header.php
+++ views/Shared/header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE11" />
     <link rel="stylesheet" href="/public/css/dark.css">
     <link rel="stylesheet" href="/public/css/styles.css">
     <link rel="stylesheet" href="/public/css/cart.css">
